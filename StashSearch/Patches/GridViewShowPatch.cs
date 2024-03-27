3ec79f22bce50ff8624b89716e2837aa52e68e6f
Cj <161484149+CJ-SPT@users.noreply.github.com>
2024-03-26 20:27:26 -0400

    Add project files.

---
+++ StashSearch/Patches/GridViewShowPatch.cs
@@ -0,0 +1,33 @@
+﻿using Aki.Reflection.Patching;
+using EFT.InventoryLogic;
+using EFT.UI;
+using EFT.UI.DragAndDrop;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace StashSearch.Patches
+{
+    internal class ContainersPanelShowPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(ContainersPanel), nameof(ContainersPanel.Show));
+        }
+
+        [PatchPostfix]
+        public static void PatchPostfix(ContainersPanel __instance, Dictionary<EquipmentSlot, SlotView> ___dictionary_0, SlotView ____defaultSlotTemplate)
+        {
+            ___dictionary_0[EquipmentSlot.SecuredContainer].gameObject.SetActive(false);
+
+            foreach (var rectTransform in ___dictionary_0[EquipmentSlot.Pockets].GetComponentsInChildren(typeof(RectTransform)))
+            {
+                if (rectTransform.gameObject.name == "Special Slots Panel Template")
+                {
+                    rectTransform.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
---
+++ StashSearch/Plugin.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using System.IO;
+using System;
+using UnityEngine;
+using System.Reflection;
+using Comfort.Common;
+using EFT.UI;
+using StashSearch.Config;
+using StashSearch.Patches;
+
+#pragma warning disable
+
+namespace StashSearch
+{
+    [BepInPlugin("com.dirtbikercj.HardcoreUI", "HardcoreUI", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Plugin? Instance;
+        public static ManualLogSource Log;
+
+        internal void Awake()
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+
+            Log = Logger;
+
+            StashSearchConfig.InitConfig(Config);
+
+
+            new ContainersPanelShowPatch().Enable();
+           
+        }
+    }
+}
